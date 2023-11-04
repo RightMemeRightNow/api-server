76a0b7154a3d1fca7ddb6eb41647aabde7944fca
Sangyeop Kim <sangykimiv@gmail.com>
2023-11-04 22:02:29 +0900

    chore: allow CORS for all

--- app/main.py
+++ app/main.py
@@ -1,10 +1,22 @@
 import pandas as pd
 from fastapi import FastAPI
 from mangum import Mangum
+from starlette.middleware.cors import CORSMiddleware
+
 from app.api.v1.api import router as api_router
 from app.api.v1.endpoints.images import data
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event('startup')
